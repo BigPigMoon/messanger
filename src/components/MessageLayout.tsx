eba6a8e324d43b7dcd97d72749737499a7327804
bigpigmoon <matvey.novikov02@gmail.com>
2023-05-20 16:30:18 +0700

    feat(pagination): finaly add pagination

--- src/components/MessageLayout.tsx
+++ src/components/MessageLayout.tsx
@@ -1,8 +1,4 @@
-import { fetcher } from "../http";
-import useSWR from "swr";
-import { UserType } from "../types";
 import Messages from "./Messages";
-import { useRef } from "react";
 
 type Props = {
   userId: number;
@@ -10,27 +6,10 @@ type Props = {
 };
 
 const MessageLayout = ({ ws, userId }: Props) => {
-  const { data: me } = useSWR<UserType>("/users/me", fetcher);
-  const { data: other } = useSWR<UserType>(
-    `/users/get?user_id=${userId}`,
-    fetcher
-  );
-
-  const scrollRef = useRef<HTMLDivElement>(null);
-
   return (
     <>
-      <div
-        className="h-full scrollbar-hidden overflow-auto space-y-3 mb-2"
-        ref={scrollRef}
-      >
-        <Messages
-          scrollRef={scrollRef}
-          userId={userId}
-          me={me}
-          other={other}
-          ws={ws}
-        />
+      <div className="h-full overflow-scroll space-y-3 mb-2">
+        <Messages userId={userId} ws={ws} />
       </div>
     </>
   );
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,19 +1,30 @@
-import { useEffect } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import useSWRInfinite from "swr/infinite";
+import useSWR from "swr";
 import { fetcher } from "../http";
 import { MessageType, UserType } from "../types";
 import ChatBubble from "./ChatBubble";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 type Props = {
   userId: number;
   me?: UserType;
   other?: UserType;
   ws: React.MutableRefObject<WebSocket | undefined>;
-  scrollRef: React.RefObject<HTMLDivElement>;
 };
 
-const Messages = ({ userId, me, other, ws, scrollRef }: Props) => {
-  const { data, mutate, setSize } = useSWRInfinite(
+const Messages = ({ userId, ws }: Props) => {
+  const [showScrollDown, setShowScrollDown] = useState(false);
+  const [firstInit, setFirstInit] = useState(true);
+  const [prevScrollBottom, setPrevScrollBottom] = useState(0);
+
+  const { data: me } = useSWR<UserType>("/users/me", fetcher);
+  const { data: other } = useSWR<UserType>(
+    `/users/get?user_id=${userId}`,
+    fetcher
+  );
+
+  const { data, mutate, size, setSize } = useSWRInfinite(
     (index) => {
       return `/messages/get?recipient_id=${userId}&limit=50&offset=${index * 50
         }`;
@@ -45,30 +56,95 @@ const Messages = ({ userId, me, other, ws, scrollRef }: Props) => {
     }
   }, [ws, me, other]);
 
-  useEffect(() => { }, []);
+  const bottomElement = useRef<HTMLDivElement>(null);
+  const outerDiv = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (data && firstInit) {
+      bottomElement.current?.scrollIntoView();
+      setFirstInit(false);
+    }
+  }, [data, firstInit]);
+
+  useEffect(() => {
+    setFirstInit(true);
+  }, [ws, other?.id]);
+
+  useEffect(() => {
+    if (outerDiv.current) {
+      const outerDivHeight = outerDiv.current.clientHeight;
+      const outerDivScrollTop = outerDiv.current.scrollTop;
+
+      if (outerDivHeight < outerDivScrollTop) {
+        bottomElement.current?.scrollIntoView();
+      } else {
+        outerDiv.current.scrollTo({
+          top: outerDiv.current.scrollHeight - prevScrollBottom,
+        });
+      }
+
+      setPrevScrollBottom(outerDiv.current.scrollHeight);
+    }
+  }, [data]);
+
+  const handleScroll = useCallback(() => {
+    if (outerDiv.current) {
+      const outerDivHeight = outerDiv.current.scrollHeight;
+      const a = outerDiv.current.clientHeight;
+      const outerDivScrollTop = outerDiv.current.scrollTop;
+
+      if (outerDivHeight !== outerDivScrollTop + a) {
+        setShowScrollDown(true);
+      } else {
+        setShowScrollDown(false);
+      }
+
+      if (outerDivScrollTop === 0) {
+        setSize((prev) => prev + 1);
+      }
+    }
+  }, []);
+
+  const handleScrollButton = useCallback(() => {
+    bottomElement.current?.scrollIntoView();
+  }, []);
 
   return (
     <>
-      <button
-        onClick={() => {
-          setSize((prev) => prev + 1);
-        }}
-        className="btn"
+      <div
+        ref={outerDiv}
+        className="h-full overflow-scroll"
+        onScroll={handleScroll}
       >
-        Load more...
-      </button>
-      {messages
-        .sort((a: MessageType, b: MessageType) => {
-          return Date.parse(a.created_at) - Date.parse(b.created_at);
-        })
-        .map((val: MessageType) => (
-          <ChatBubble
-            key={val.id}
-            left={val.sender_id === userId}
-            message={val.message_text}
-            createdAt={val.created_at}
-          />
-        ))}
+        <div className="space-y-3 mb-2">
+          {messages
+            .sort((a: MessageType, b: MessageType) => {
+              return Date.parse(a.created_at) - Date.parse(b.created_at);
+            })
+            .map((val: MessageType) => (
+              <ChatBubble
+                key={val.id}
+                left={val.sender_id === userId}
+                message={val.message_text}
+                createdAt={val.created_at}
+              />
+            ))}
+          <div ref={bottomElement} />
+        </div>
+      </div>
+      {showScrollDown && (
+        <div className="indicator absolute bottom-24 right-0 m-10 z-30">
+          {/* {newMessage && ( */}
+          {/*   <span className="indicator-item indicator-center badge badge-secondary" /> */}
+          {/* )} */}
+          <button
+            onClick={handleScrollButton}
+            className="btn btn-circle place-items-center"
+          >
+            <ChevronDownIcon className="h-6 w-6 text-gray-500" />
+          </button>
+        </div>
+      )}
     </>
   );
 };
